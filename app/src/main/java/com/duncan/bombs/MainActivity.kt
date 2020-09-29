9ffcb9b9494d3244c4de2278a21f4253f247b6d2
Michael Duncan <mwduncan2018@gmail.com>
2020-09-28 22:20:12 -0400

    updates

--- app/src/main/java/com/duncan/bombs/MainActivity.kt
+++ app/src/main/java/com/duncan/bombs/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.duncan.bombs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
+import android.view.View
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+const val EXTRA_MESSAGE = "com.duncan.bombs.MESSAGE"
+
+class MainActivity() : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
 }
